b6df5c893ab9cb2422451d0bf83a9e62daad21d0
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-11-01 13:21:35 +0500

    feat: implement getProductsList and getProductsById functions

--- product-service/handlers/getProductsById.ts
+++ product-service/handlers/getProductsById.ts
@@ -1,12 +1,30 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 
-export const getProductsById: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      id: 1,
-      name: "Prod 1",
-    }),
-  };
+import productsList from "./products.json";
+// simulate db connection in order to use async/await in lambda
+const productsPromise = Promise.resolve(productsList);
+
+export const getProductsById: APIGatewayProxyHandler = async (event) => {
+  try {
+    const { productId } = event.pathParameters;
+    const productsData = await productsPromise;
+    const product = productsData.find((item) => item.id === productId);
+
+    if (product) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product),
+      };
+    }
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Product not found" }),
+    };
+  } catch {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Server error: Cannot get product by id" }),
+    };
+  }
 };
--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -1,18 +1,23 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 
+import productsList from "./products.json";
+// simulate db connection in order to use async/await in lambda
+const productsPromise = Promise.resolve(productsList);
+
 export const getProductsList: APIGatewayProxyHandler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify([
-      {
-        id: 1,
-        name: "Prod 1",
-      },
-      {
-        id: 2,
-        name: "Prod 2",
-      },
-    ]),
-  };
+  try {
+    const productsData = await productsPromise;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(productsData),
+    };
+  } catch {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Server error: Cannot get list of products",
+      }),
+    };
+  }
 };
