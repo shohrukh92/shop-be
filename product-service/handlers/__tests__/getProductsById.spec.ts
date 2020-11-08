ca3a3ce7a505eb89edc9c3fa0c2ca0017c8fc3f0
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-11-08 23:48:53 +0500

    feat: fix unit tests

--- product-service/handlers/__tests__/getProductsById.spec.ts
+++ product-service/handlers/__tests__/getProductsById.spec.ts
@@ -1,31 +1,52 @@
-import { APIGatewayProxyResult } from "aws-lambda";
-
+import { Client } from "pg";
 import { getProductsById } from "../getProductsById";
 
-jest.mock("../products.json", () => [{ id: 1 }]);
+jest.mock("pg", () => {
+  const clientMock = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn(),
+  };
+  return { Client: jest.fn(() => clientMock) };
+});
 
 describe("getProductsById", () => {
-  test("should return product by id with status 200 and allowed all origins", async () => {
-    const eventMock = { pathParameters: { productId: 1 } } as any;
-    const result = (await getProductsById(
-      eventMock,
-      null,
-      null
-    )) as APIGatewayProxyResult;
-
-    expect(result.statusCode).toBe(200);
-    expect(result.body).toEqual(JSON.stringify({ id: 1 }));
-    expect(result.headers).toEqual({ "Access-Control-Allow-Origin": "*" });
+  let client;
+  beforeEach(() => {
+    client = new Client();
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
-  test("should return 404 not found error if product id does not exist", async () => {
-    const eventMock = { pathParameters: { productId: 2 } } as any;
-    const result = (await getProductsById(
-      eventMock,
-      null,
-      null
-    )) as APIGatewayProxyResult;
+  test("should return 404 product not found", async () => {
+    client.query.mockResolvedValueOnce({ rows: [] });
+    const eventMock = { pathParameters: { productId: 1 } } as any;
+    const result = await getProductsById(eventMock, null, null);
 
-    expect(result.statusCode).toBe(404);
+    expect(client.connect).toBeCalledTimes(1);
+    expect(client.query).toBeCalled();
+    expect(client.end).toBeCalledTimes(1);
+    expect(result).toEqual({
+      statusCode: 404,
+      body: '{"error":"Product not found in db"}',
+      headers: { "Access-Control-Allow-Origin": "*" },
+    });
+  });
+
+  test("should return existing product with status 200", async () => {
+    const productsMock = [{ id: 1, title: "Test" }];
+    client.query.mockResolvedValueOnce({ rows: productsMock });
+    const eventMock = { pathParameters: { productId: 1 } } as any;
+    const result = await getProductsById(eventMock, null, null);
+
+    expect(client.connect).toBeCalledTimes(1);
+    expect(client.query).toBeCalled();
+    expect(client.end).toBeCalledTimes(1);
+    expect(result).toEqual({
+      statusCode: 200,
+      body: JSON.stringify(productsMock[0]),
+      headers: { "Access-Control-Allow-Origin": "*" },
+    });
   });
 });
