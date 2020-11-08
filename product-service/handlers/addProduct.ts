dc074fcee5eb04c909d870f670af7d9bd80e7fad
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-11-08 22:27:01 +0500

    feat: validate product before inserting into db

--- product-service/handlers/addProduct.ts
+++ product-service/handlers/addProduct.ts
@@ -3,12 +3,23 @@ import "source-map-support/register";
 import { Client } from "pg";
 import { generateResponse } from "./utils";
 import { dbOptions } from "./dbOptions";
+import { Product, validateProduct } from "./productSchema";
 
 export const addProduct: APIGatewayProxyHandler = async (event) => {
   console.log(event);
   let client;
   try {
-    const { title, description, price, count } = JSON.parse(event.body);
+    const product: Product = JSON.parse(event.body);
+    const validation = validateProduct(product);
+
+    if (validation) {
+      return generateResponse({
+        code: 400,
+        body: validation.errors,
+      });
+    }
+
+    const { title, description, price, count } = product;
     client = new Client(dbOptions);
     await client.connect();
 
@@ -18,14 +29,15 @@ export const addProduct: APIGatewayProxyHandler = async (event) => {
       "insert into products (title, description, price) values ($1, $2, $3) returning *",
       [title, description, price]
     );
-
     await client.query(
       "insert into stocks (product_id, count) values ($1, $2)",
       [insertedProductRes.rows[0].id, count]
     );
 
     await client.query("commit"); // transaction ends
+
     return generateResponse({
+      code: 201,
       body: {
         ...insertedProductRes.rows[0],
         count,
