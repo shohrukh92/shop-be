a3ec7b205c165e5428727caaedba6e44622ed6bc
Shohrukh Davlatov <shohrukh@inspectorio.com>
2020-11-08 13:30:45 +0500

    feat: add ddl and initial dml sql queries

---
+++ product-service/sql/products.initial.dml.sql
@@ -0,0 +1,11 @@
+insert into products
+(title, description, price) values
+('Eloquent JavaScript', 'A Modern Introduction to Programming', 2.4),
+('Learning JavaScript Design Patterns', 'A JavaScript and jQuery Developer Guide', 10),
+('Speaking JavaScript', 'An In-Depth Guide for Programmers', 23);
+
+insert into stocks 
+(product_id, count) values
+('2f7e7680-2cb2-4f79-8cdb-fc2aab269cda', 20),
+('48bdf363-5333-4649-a425-e02df4bc520c', 10),
+('e8b3f8c3-ff2f-4751-909e-37b1051fdd3d', 30);
